931fb9612751d9b36b23e01331bcd56a77714caf
agenosa <agenosa@my.bcit.ca>
2022-05-19 12:46:14 -0700

    attempting to fix 404 error

--- src/App.js
+++ src/App.js
@@ -20,17 +20,16 @@ function App() {
     <Container main>
       <GlobalStyles />
       <StyledNavBar />
-
       <Switch>
       <Route exact path="/"><Home /></Route>
-      <Route path="/about"><About /></Route>
-      <Route path="/contact"><Contact /></Route>
-      <Route path="/projects"><Projects /></Route>
-      <Route path="/vueproject"><VueProject /></Route>
-      <Route path="/reactproject"><ReactProject /></Route>
-      <Route path="/csharpproject"><CSharpProject /></Route>
-      <Route path="/angularproject"><AngularProject /></Route>
-      <Route path="/whatsonpresale"><ProjectP3 /></Route>
+      <Route exact path="/about"><About /></Route>
+      <Route exact path="/contact"><Contact /></Route>
+      <Route exact path="/projects"><Projects /></Route>
+      <Route exact path="/vueproject"><VueProject /></Route>
+      <Route exact path="/reactproject"><ReactProject /></Route>
+      <Route exact path="/csharpproject"><CSharpProject /></Route>
+      <Route exact path="/angularproject"><AngularProject /></Route>
+      <Route exact path="/whatsonpresale"><ProjectP3 /></Route>
       </Switch>
 
       <StyledFooter footerLabel="Created by Adrian Genosa &copy;2022 &#8594; adrian.genosa@gmail.com" />
